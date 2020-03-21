258c970a55978884dd9ac8fa1c0f6c8464a6847c
Kausik N <nkausik1999@gmail.com>
2020-03-21 14:21:32 +0530

    added generic function for parsing

--- GigaCode.py
+++ GigaCode.py
@@ -95,15 +95,127 @@ def ExtractFunctionsFromFile_Python(file_path='', file_obj=None, tabSpace=4):
 	return Functions
 
 
+class PythonParams:
+	def __init__(self):
+		self.DescriptionMarks_Start = ['"""', "'''"]
+		self.DescriptionMarks_End = ['"""', "'''"]
+		self.CommentRegeX = '^#.*'
+		self.FunctionNameRegeX = '^def([^(]*)\('
+		self.FunctionParamsRegeX = '^def[^(]*\((.*)\)'
+		self.tabAlernateSpaces = ' ' * 4
+	
+	def InFunctionBlockCheck(self, line):
+		return (line.startswith('\t') or line.startswith(self.tabAlernateSpaces))
+
+	def NewFunctionCheck(self, line):
+		return line.startswith('def')
+
+
+def GetLanguageParams(language):
+	params = None
+	if language == 'Python':
+		params = PythonParams()
+		return params
+
+
+def ExtractFunctionsFromFile(file_path='', file_obj=None, language='Python', tabSpace=4):
+	''' Extracts Functions (NOT CLASS FUNCTIONS) from a python file '''
+	Functions = []
+
+	LanguageParams = GetLanguageParams(language)
+
+	# Read Text of File
+	if file_obj == None:
+		file_obj = open(file_path, 'r')
+	
+	file_text = file_obj.read()
+
+	# Split by \n
+	code_lines = file_text.split('\n')
+
+	# Loop and identify functions
+	tabAlernateSpaces = ' ' * tabSpace
+	LanguageParams.tabAlernateSpaces = tabAlernateSpaces
+	InFunction = False
+	InFunctionDescEnd = None
+	curFunctionName = ''
+	curFunctionParams = []
+	curFunctionLines = []
+	curFunctionDesc = ''
+	for line in code_lines:
+		if InFunction:
+			# Check if Description Line
+			stripped_line = re.findall('^\t*(.*)', line.strip())[0]
+			if not InFunctionDescEnd == None:
+				if stripped_line.endswith(InFunctionDescEnd):
+					InFunctionDescEnd = None
+					curFunctionDesc += stripped_line[:-len(InFunctionDescEnd)]
+					continue
+				else:
+					curFunctionDesc += line
+					continue
+			
+			MarkCheck = False
+			for descmarkstart, descmarkend in zip(LanguageParams.DescriptionMarks_Start, LanguageParams.DescriptionMarks_End):
+				if stripped_line.startswith(descmarkstart):
+					InFunctionDescEnd = descmarkend
+					curFunctionDesc += stripped_line[len(descmarkstart):]
+					stripped_line = stripped_line[len(descmarkstart):]
+					if stripped_line.endswith(InFunctionDescEnd):
+						InFunctionDescEnd = None
+						curFunctionDesc = curFunctionDesc[:-len(descmarkstart)]
+					MarkCheck = True
+					break
+			if MarkCheck:
+				continue
+
+			# Check if in function block
+			# If Comment always add to function code lines
+			if line.strip() == '' or (LanguageParams.InFunctionBlockCheck(line)) or not re.search(LanguageParams.CommentRegeX, line.strip()) == None:
+				#print("curapp:", line)
+				curFunctionLines.append(line)
+				continue
+			# If not within function block, end function lines
+			else:
+				#print("curclose:", line)
+				curFunction = {}
+				curFunction['Name'] = curFunctionName.strip()
+				curFunction['Parameters'] = curFunctionParams
+				curFunction['Code'] = curFunctionLines
+				curFunction['Description'] = curFunctionDesc.strip()
+				Functions.append(curFunction)
+				curFunctionName = ''
+				curFunctionParams = []
+				curFunctionLines = []
+				curFunctionDesc = ''
+				InFunction = False
+		# New function check
+		if LanguageParams.NewFunctionCheck(line):
+			#print("funcstart:", line)
+			InFunction = True
+			curFunctionName = ''
+			curFunctionParams = []
+			curFunctionLines = []
+			curFunctionDesc = ''
+			curFunctionLines.append(line)
+			# Find name and parameters of function
+			curFunctionName = re.findall(LanguageParams.FunctionNameRegeX, line.strip())[0].strip()
+			curFunctionParams = (re.findall(LanguageParams.FunctionParamsRegeX, line.strip())[0].strip()).split(',')
+			for pi in range(len(curFunctionParams)):
+				curFunctionParams[pi] = curFunctionParams[pi].strip()
+
+	return Functions
+
+
 
 # Driver Code
-# Functions = ExtractFunctionsFromFile_Python('GigaCode.py', tabSpace=4)
+Functions = ExtractFunctionsFromFile('GigaCode.py', language='Python', tabSpace=4)
 # import pickle
 # Functions = pickle.load(open('FunctionDatabases/PythonTestDB.p', 'rb'))
-# for f in Functions:
-# 	print('\n\n')
-# 	print("Name:", f['Name'])
-# 	print("Desc:", f['Description'])
-# 	print("Parameters:", f['Parameters'])
-# 	for fl in f['Code']:
-# 		print(fl)
+for f in Functions:
+	print('\n\n')
+	print("Name:", f['Name'])
+	print("Desc:", f['Description'])
+	print("Parameters:", f['Parameters'])
+	for fl in f['Code']:
+		print(fl)
